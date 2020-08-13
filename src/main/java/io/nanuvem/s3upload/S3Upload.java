0e86182165c687303df18a9d496e795c20538755
Vinnicius S <niknicius@gmail.com>
2020-08-13 18:24:40 -0300

    feat: initial commit

---
+++ src/main/java/io/nanuvem/s3upload/S3Upload.java
@@ -0,0 +1,13 @@
+package io.nanuvem.s3upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3Upload {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3Upload.class, args);
+    }
+
+}
